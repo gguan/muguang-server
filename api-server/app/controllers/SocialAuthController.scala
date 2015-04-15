27f2b99da4383501bb332955e8c8f6332d10b501
Guan Guan <guanguan1114@gmail.com>
2015-04-16 01:40:38 +0800

    Implement testing /api/v1/authenticate/:provider API

--- api-server/app/controllers/SocialAuthController.scala
+++ api-server/app/controllers/SocialAuthController.scala
@@ -7,12 +7,14 @@ import com.mohiva.play.silhouette.api.exceptions.ProviderException
 import com.mohiva.play.silhouette.api.services.AuthInfoService
 import com.mohiva.play.silhouette.impl.authenticators.SessionAuthenticator
 import com.mohiva.play.silhouette.impl.providers._
+import com.muguang.util.RandomUtils
 import models.User
 import models.user.UserService
 import play.api.i18n.Messages
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.json.Json
 import play.api.mvc.Action
+import utils.oauth2.{ WeiboProvider, WeiboProfileBuilder }
 
 import scala.concurrent.Future
 
@@ -60,4 +62,53 @@ class SocialAuthController @Inject() (
         BadRequest(Json.obj("error" -> Messages("could.not.authenticate")))
     }
   }
+
+  def verifySocialAuth(provider: String) = Action.async(parse.json) { implicit request =>
+
+    val idOpt = (request.body \ "uid").asOpt[String]
+    val authInfoOpt = (request.body \ "oauth2_info").asOpt[OAuth2Info]
+
+    (idOpt, authInfoOpt) match {
+      case (Some(id), Some(authInfo)) => {
+        val params = authInfo.params.getOrElse(Map[String, String]()) + ("uid" -> id)
+        val authInfoWithId = authInfo.copy(params = Some(params))
+
+        (env.providers.get(provider) match {
+          case Some(p: WeiboProvider with WeiboProfileBuilder) =>
+            for {
+              profile <- p.retrieveProfile(authInfoWithId)
+              user <- userService.save(profile)
+              authInfo <- authInfoService.save(profile.loginInfo, authInfo)
+              authenticator <- env.authenticatorService.create(user.loginInfo)
+              value <- env.authenticatorService.init(authenticator)
+              result <- env.authenticatorService.embed(value, Future.successful(
+                // TODO
+                Ok("authenticate successfully.")
+              ))
+            } yield {
+              env.eventBus.publish(LoginEvent(user, request, request2lang))
+              result
+            }
+          case _ => Future.failed(new ProviderException(s"Cannot authenticate with unexpected social provider $provider"))
+        }).recover {
+          case e: ProviderException =>
+            logger.error("Unexpected provider error", e)
+            BadRequest(Json.obj("error" -> Messages("could.not.authenticate")))
+        }
+
+        // TODO:
+
+        Future.successful(Ok(Json.obj(
+          "id" -> id,
+          "access_token" -> ""
+        )))
+      }
+      case _ => {
+        Future.successful(BadRequest(Json.obj(
+          "error" -> "missing id or access_token"
+        )))
+      }
+    }
+  }
+
 }
